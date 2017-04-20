5e17c8d4b683dd53c1cf2a5daa2dbca5932948cd
Sofia Atsalou <sofia.atsalou@gmail.com>
2017-04-20 17:36:30 +0300

    feat(logger) Implement the Logger service

---
+++ src/services/logger.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { SlackService } from './slack.service';
+
+
+@Injectable()
+export class LoggerService {
+  private slackService: SlackService;
+
+	constructor(slackService: SlackService) {
+		this.slackService = slackService;
+	}
+
+	public info(message): void {
+    let label = 'Info: ';
+    let color = '#3AA3E3';
+    message = label.concat(message);
+		this.slackService.post(message, color).subscribe();
+  }
+
+  public log(message): void {
+    let label = 'Log: ';
+    let color = '#3AE345';
+    message = label.concat(message);
+		this.slackService.post(message, color).subscribe();
+  }
+
+  public warn(message): void {
+    let label = 'Warn: ';
+    let color = '#E2E33A';
+    message = label.concat(message);
+		this.slackService.post(message, color).subscribe();
+  }
+
+  public error(message): void {
+    let label = 'Error: ';
+    let color = '#E33A43';
+    message = label.concat(message);
+		this.slackService.post(message, color).subscribe();
+  }
+}
