34a130d1030f3cd54fd58574f81ec4fe92bf6945
Sofia Atsalou <sofia.atsalou@gmail.com>
2017-04-20 17:35:10 +0300

    feat(slack) Set the incoming slack webhook url in config

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,5 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class Config {
 	public wordpressApiUrl = 'http://demo.titaniumtemplates.com/wordpress/?json=1';
+	static slackIncomingWebHookUrl = 'https://hooks.slack.com/services/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';
 }
