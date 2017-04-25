038f6cc5a49b7e596a982ee3b5dfd851644cfef2
Sofia Atsalou <sofia.atsalou@gmail.com>
2017-04-25 15:04:07 +0300

    feat(logger-service) Log the response of the observable subscribe operator

--- src/services/logger.service.ts
+++ src/services/logger.service.ts
@@ -14,27 +14,27 @@ export class LoggerService {
     let label = 'Info: ';
     let color = '#3AA3E3';
     message = label.concat(message);
-		this.slackService.post(message, color).subscribe();
+		this.slackService.post(message, color).subscribe(res => console.log(res));
   }
 
   public log(message): void {
     let label = 'Log: ';
     let color = '#3AE345';
     message = label.concat(message);
-		this.slackService.post(message, color).subscribe();
+		this.slackService.post(message, color).subscribe(res => console.log(res));
   }
 
   public warn(message): void {
     let label = 'Warn: ';
     let color = '#E2E33A';
     message = label.concat(message);
-		this.slackService.post(message, color).subscribe();
+		this.slackService.post(message, color).subscribe(res => console.log(res));
   }
 
   public error(message): void {
     let label = 'Error: ';
     let color = '#E33A43';
     message = label.concat(message);
-		this.slackService.post(message, color).subscribe();
+		this.slackService.post(message, color).subscribe(res => console.log(res));
   }
 }
